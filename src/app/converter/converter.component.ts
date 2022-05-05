663dfea6b0e0a2f1f67a920220d8d71f4ad8b36c
Wei78 <coderedxd03@gmail.com>
2022-05-05 14:05:35 +0300

    Update converter.component.ts

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -36,73 +36,29 @@ export class ConverterComponent implements OnInit {
       (i) => i.base_ccy === fromCurrency && i.ccy === toCurrecny
     );
     if (details) {
-      console.log('called1');
       return this.directConvert(value.target.value, details.buy);
     }
     details = this.currencies.find(
       (i) => i.ccy === fromCurrency && i.base_ccy === toCurrecny
     );
     if (details) {
-      console.log('called2');
       return this.reverseConvert(value.target.value, details.buy);
     }
     const middleDetails = this.currencies.find(
       (i) => i.ccy === fromCurrency && i.base_ccy === base
     );
-    console.log('called3')
+
     details = this.currencies.find(
       (i) => i.ccy === toCurrecny && i.base_ccy === base
     );
-    console.log('called4')
+
     if (middleDetails && details) {
-      console.log('called5');
       const middleValue = this.reverseConvert(value.target.value, middleDetails.buy);
       return this.directConvert(middleValue, details.buy);
     }
-    console.log('called6');
+
     return NaN;
   };
-
-
-  
-  // changeTop(top: any): void {
-
-  //   this.currencies.find(this.optionTop);
-    
-  //   if (this.optionTop === "UAH" && this.optionBottom === "USD") {
-  //     this.currencyBottom = (top.target.value / +this.currencies[0].buy).toFixed(3);
-  //   } else if (this.optionTop === "UAH" && this.optionBottom === "EUR") {
-  //     this.currencyBottom = (top.target.value / +this.currencies[1].buy).toFixed(3);
-  //   } else if (this.optionTop === "USD" && this.optionBottom === "EUR") {
-  //     this.currencyBottom = ((+this.currencies[1].buy * top.target.value)/+this.currencies[0].buy).toFixed(3)
-  //   } else if (this.optionTop === "EUR" && this.optionBottom === "USD") {
-  //     this.currencyBottom = ((+this.currencies[0].buy * top.target.value)/+this.currencies[1].buy).toFixed(3)
-  //   } else if (this.optionTop === "USD" && this.optionBottom === "UAH") {
-  //     this.currencyBottom = (top.target.value * +this.currencies[0].buy).toFixed(3);
-  //   } else if (this.optionTop === "EUR" && this.optionBottom === "UAH") {
-  //     this.currencyBottom = (top.target.value * +this.currencies[1].buy).toFixed(3);
-  //   } else {
-  //     this.currencyBottom = top.target.value
-  //   }
-  // }
-
-  // changeBottom(bottom: any): void {
-  //   if (this.optionBottom === "UAH" && this.optionTop === "USD") {
-  //     this.currencyTop = (bottom.target.value / +this.currencies[0].buy).toFixed(3);
-  //   } else if (this.optionBottom === "UAH" && this.optionTop === "EUR") {
-  //     this.currencyTop = (bottom.target.value / +this.currencies[1].buy).toFixed(3);
-  //   } else if (this.optionBottom === "USD" && this.optionTop === "EUR") {
-  //     this.currencyTop = ((+this.currencies[1].buy * bottom.target.value)/+this.currencies[0].buy).toFixed(3)
-  //   } else if (this.optionBottom === "EUR" && this.optionTop === "USD") {
-  //     this.currencyTop = ((+this.currencies[0].buy * bottom.target.value)/+this.currencies[1].buy).toFixed(3)
-  //   } else if (this.optionBottom === "USD" && this.optionTop === "UAH") {
-  //     this.currencyTop = (bottom.target.value * +this.currencies[0].buy).toFixed(3);
-  //   } else if (this.optionBottom === "EUR" && this.optionTop === "UAH") {
-  //     this.currencyTop = (bottom.target.value * +this.currencies[1].buy).toFixed(3);
-  //   } else {
-  //     this.currencyTop = bottom.target.value
-  //   }
-  // }
 
   ngOnInit(): void {
     
